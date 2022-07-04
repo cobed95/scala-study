2fcdfd2d76655dbae9971f3809d77836b601b207
Eundo Lee <cobed95@gmail.com>
2022-07-04 14:08:10 +0900

    Revert "Revert "[FEAT] Add up to P17 and add project""
    This reverts commit aa8e2ea020468c66c2e677f0d36b99700157087e.

---
+++ src/main/scala/problems99/lists/P08.scala
@@ -0,0 +1,14 @@
+package org.laplacetec.study
+package problems99.lists
+
+object P08 {
+  /**
+   * (**) Eliminate consecutive duplicates of list elements.
+   * If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed.
+   * Example:
+   *    scala> compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+   *    res0: List[Symbol] = List('a, 'b, 'c, 'a, 'd, 'e)
+   * @return
+   */
+  def solution[A <: Equals]: List[A] => List[A] = ???
+}
