af1ab5e6e31d11faeb5c8350a21462471717ac6d
Lukas Widmer <luky.widmer@gmail.com>
2023-04-17 15:04:23 +0900

    problem 61 part 2 to 64

--- src/main/scala/problems99/Tree.scala
+++ src/main/scala/problems99/Tree.scala
@@ -5,6 +5,12 @@ sealed abstract class Tree[+T] {
   def isSymmetric: Boolean
 
   def leafCount: Int
+
+  def leafList: List[T]
+
+  def internalList: List[T]
+
+  def atLevel: Int => List[T]
 }
 
 case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
@@ -14,12 +20,56 @@ case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
     case (End, End) => 1
     case (_, _)     => left.leafCount + right.leafCount
   }
+
+  def leafList: List[T] = (left, right) match {
+    case (End, End) => List(value)
+    case (_, _)     => left.leafList ::: right.leafList
+  }
+
+  def internalList: List[T] = (left, right) match {
+    case (End, End) => Nil
+    case (_, _)     => value :: left.internalList ::: right.internalList
+  }
+
+  def atLevel: Int => List[T] = { x =>
+    (left, right) match {
+      case (End, End)       => Nil
+      case (_, _) if x == 1 => List(value)
+      case (_, _)           => left.atLevel(x - 1) ::: right.atLevel(x - 1)
+    }
+  }
+
+  def layoutBinaryTree: Tree[T] = {
+    def layoutBinaryTree: (Int, Int) => (Tree[T], Int) = { (x, y) =>
+      val (leftPositionedNode, leftPositionedNodeHighestX) =
+        layoutBinaryTree(x, y + 1)
+      val (rightPositionedNode, rightPositionedNodeHighestX) =
+        layoutBinaryTree(leftPositionedNodeHighestX + 2, y + 1)
+      (
+        PositionedNode(
+          value,
+          leftPositionedNode,
+          rightPositionedNode,
+          leftPositionedNodeHighestX + 1,
+          y
+        ),
+        rightPositionedNodeHighestX
+      )
+    }
+    layoutBinaryTree(1, 1)._1
+  }
 }
 
 case object End extends Tree[Nothing] {
   def isSymmetric: Boolean = true
 
   def leafCount: Int = 0
+
+  def leafList: List[Nothing] = Nil
+
+  def internalList: List[Nothing] = Nil
+
+  def atLevel: Int => List[Nothing] = Nil
 }
 
 object Tree {
@@ -78,4 +128,19 @@ object Tree {
       sameHeightTrees ::: differentHeightTrees
     }
   }
+
+  def completeBinaryTree[T]: (Int, T) => Tree[T] = { (x, y) =>
+    def treeByAddress: Int => Tree[T] = {
+      case z if z > x => End
+      case z          => Node(y, treeByAddress(2 * z), treeByAddress(2 * z + 1))
+    }
+    treeByAddress(1)
+  }
 }
+
+case class PositionedNode[+T](override val value: T,
+                              override val left: Tree[T],
+                              override val right: Tree[T],
+                              x: Int,
+                              y: Int)
+    extends Node[T](value, left, right)
